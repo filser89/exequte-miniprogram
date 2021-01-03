3b92470552d6a8b141177bd88468397f7d5a5bc3
Sergey Filippov <sergey.filippov.89@inbox.ru>
2021-01-03 19:48:00 +0800

    my-classes fixxed

---
+++ pages/my-classes/components/BookNowButton/BookNowButton.js
@@ -0,0 +1,6 @@
+// pages/my-classes/components/BookNowButton/BookNowButton.js
+import ActionButtonBehavior from "../../../../behaviors/action-button"
+ Component({
+
+  behaviors: [ActionButtonBehavior]
+ })
