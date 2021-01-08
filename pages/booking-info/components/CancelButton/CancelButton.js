f817f78ff0c878fa17d02a120848ba7eea336b8d
Sergey Filippov <sergey.filippov.89@inbox.ru>
2021-01-09 06:46:36 +0800

    made proper cancel btn on booking ino page

---
+++ pages/booking-info/components/CancelButton/CancelButton.js
@@ -0,0 +1,5 @@
+// pages/booking-info/components/CancelButton/CancelButton.js
+import ActionButtonBehavior from "../../../../behaviors/action-button"
+ Component({
+  behaviors: [ActionButtonBehavior]
+ })
