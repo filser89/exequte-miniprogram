1448b1fb60a64ece5ad11d5db183e746507a7e1f
Ekaterina Gorbacheva <ekagorbacheva@mail.ru>
2021-01-14 14:48:03 +0700

    added share and cancel card on class info fixed

--- pages/booking/booking.js
+++ pages/booking/booking.js
@@ -172,5 +172,42 @@ Page({
         })
     }
     console.log('btnPattern', this.data.btnPattern)
+  },
+
+/**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
   }
+
+
 })
